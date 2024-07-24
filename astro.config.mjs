a491b4cc5e87679772c97301536e950e051ded58
Diego Perez <dyego79@gmail.com>
2024-07-24 19:57:47 -0300

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-//import netlify from "@astrojs/netlify";
 
-import vercel from "@astrojs/vercel/serverless";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: vercel(),
+  adapter: netlify(),
   experimental: {
     actions: true,
   },
