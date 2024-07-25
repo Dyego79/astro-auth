de8b3586bf2bd95a256936b76964ca69d01f35b2
Diego Perez <dyego79@gmail.com>
2024-07-25 09:47:55 -0300

    Deploy Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import netlify from "@astrojs/netlify";
-
-//import vercel from "@astrojs/vercel/serverless";
+//import netlify from "@astrojs/netlify";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: netlify(),
-  //adapter: vercel(),
+  //adapter: netlify(),
+  adapter: vercel(),
   experimental: {
     actions: true,
   },
--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,7 +1,7 @@
 import { loginUser, loginWithGoogle, logout, registerUser } from "./auth";
 
 export const server = {
-  //acciones
+  //actions
   registerUser,
   logout,
   loginUser,
